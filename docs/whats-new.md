de057b5ba8b98a7d280299df90fc33d455a4f660
Simon Teng <tengsimon@hotmail.com>
2025-08-08 14:18:55 +0800

    01 added build script to copy _layouts and whats-new.md to the docs folder

---
+++ docs/whats-new.md
@@ -0,0 +1,10 @@
+---
+title: Changelog
+---
+
+# What's New
+
+### Version: 0.0.1 (2025-08-05 2000 UTC)
+* Initial Release
+
+-----
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'src/_layouts',
+                    dest: '' // This copies the folder to the root of the build directory
+                },
+                {
+                    src: 'whats-new.md',
+                    dest:''
+                }
+            ]
+        })
+    ],
     base: '/MiMaPwd/', // This is crucial for GitHub Pages to find your assets
     build: {
         outDir: 'docs' // This line tells Vite to output to the 'docs' folder
